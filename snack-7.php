29a4b95a07e1518b1d4bf939dcabe2695871cf88
iLBigga <Giannanti1996@gmail.com>
2022-09-22 01:38:53 +0200

    Snack-7 completato

--- snack-7.php
+++ snack-7.php
@@ -1,52 +1,67 @@
 <?php
 
 $studenti = [
-    'alunno1' => [
-        'name' => 'Mario',
-        'cognome' => 'Rossi',
-        'voti' => [ 
-            'matematica' => 4, 
-            'fisica' => 8, 
-            'inglese' => 3, 
-            'latino' => 7,
+        [
+            'nome' => 'Mario',
+            'cognome' => 'Rossi',
+            'voti' => [ 
+                'matematica' => 4, 
+                'fisica' => 8, 
+                'inglese' => 3, 
+                'latino' => 7,
+            ],
+
         ],
-    ],
-    'alunno2' => [
-        'name' => 'Dario',
-        'cognome' => 'Manforti',
-        'voti' => [
-            'matematica' => 2, 
-            'fisica' => 9, 
-            'inglese' => 7, 
-            'latino' => 3,
-         ],
-    ],
-    'alunno3' => [
-        'name' => 'Sara',
-        'cognome' => 'Ricci',
-        'voti' => [ 
-            'matematica' => 8, 
-            'fisica' => 10, 
-            'inglese' => 3, 
-            'latino' => 6,
-         ],
-    ],
-    'alunno4' => [
-        'name' => 'Giulia',
-        'cognome' => 'Sansone',
-        'voti' => [ 
-            'matematica' => 7, 
-            'fisica' => 6, 
-            'inglese' => 3, 
-            'latino' => 2,
-         ],
-    ],
-    
+        [
+            'nome' => 'Dario',
+            'cognome' => 'Manforti',
+            'voti' => [
+                'matematica' => 2, 
+                'fisica' => 9, 
+                'inglese' => 7, 
+                'latino' => 3,
+                ],
+        ],
+        [
+            'nome' => 'Sara',
+            'cognome' => 'Ricci',
+            'voti' => [ 
+                'matematica' => 8, 
+                'fisica' => 10, 
+                'inglese' => 3, 
+                'latino' => 6,
+                ],
+        ],
+        [
+            'nome' => 'Giulia',
+            'cognome' => 'Sansone',
+            'voti' => [ 
+                'matematica' => 7, 
+                'fisica' => 6, 
+                'inglese' => 3, 
+                'latino' => 2,
+            ],
+        ],
 ];
-
-echo var_dump($studenti);
-
 
 ?>
 
 
+<div>
+    <ul>
+        <?php
+           for($i = 0; $i < count($studenti); $i++){
+                $name = $studenti[$i]['nome'] . ' ' . $studenti[$i]['cognome'];
+                $avarage = round(array_sum($studenti[$i]['voti'])/count($studenti[$i]['voti']));
+
+        ?>
+            <li>
+                <?php
+                    echo "{$name} : {$avarage}"
+                ?>
+            </li>
+        <?php    
+           }
+        ?>
+    </ul>
+</div>
